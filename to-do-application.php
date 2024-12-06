0dee9ecb5946b2bef1c42dac56f9925ec1e9c020
Mahyar <mahyarstate@gmail.com>
2024-12-07 00:57:59 +0330

    add primary structure

---
+++ to-do-application.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>To-Do Application</title>
+</head>
+
+<body>
+    <h1>To-Do Application</h1>
+    
+    <form>
+        <label for="task">Add your task</label>
+        <input type="text" name="text">
+        <input type="submit" name="add" value="Add">
+    </form>
+
+</body>
+
+</html>
