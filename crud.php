39cb632c787f27bd5b6e174adb6595f9baf1f773
Mahyar <mahyarstate@gmail.com>
2024-12-08 19:47:23 +0330

    expand and style

--- crud.php
+++ crud.php
@@ -1,18 +1,19 @@
 <?php
-function create($name)
+function create($name, $description)
 {
     $con = new PDO("mysql:hostname=localhost;dbname=to_do_app", "root", "");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     try {
-        if ($_SERVER["REQUEST_METHOD"] === "POST" && !empty($_POST["text"])) {
+        if ($_SERVER["REQUEST_METHOD"] === "POST" && !empty($_POST["name"])) {
 
             $stmt = $con->prepare("
-            INSERT INTO tasks
-            VALUES (:name)
+            INSERT INTO tasks (name, description, status_id)
+            VALUES (:name, :description, 1)
             ");
 
             $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":description", $description);
 
             $stmt->execute();
 
@@ -32,17 +33,35 @@ function read()
 
     try {
         $stmt = $con->prepare("
-            SELECT *
+            SELECT id, name, description, status_name
             FROM tasks
+            JOIN status
+            WHERE tasks.status_id = status.status_id;
         ");
 
         $stmt->execute();
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
         $data = $stmt->fetchAll();
 
+        echo "<table>";
+
+        echo "<tr> <th> ID </th> <th> Name </th> <th> Description </th> <th> Status </th> <th> Options </th> </tr>";
+
         foreach ($data as $d) {
-            echo $d["name"] . "<br>";
+            echo "<tr>";
+            echo "<td>" . $d["id"] . "</td>";
+            echo "<td>" . $d["name"] . "</td>";
+            echo "<td>" . $d["description"] . "</td>";
+            echo "<td>" . $d["status_name"] . "</td>";
+            echo
+            "<td> 
+            <a href=#>Update</a>
+            <a href=#>Delete</a>
+            </td>";
+            echo "</tr>";
         }
+
+        echo "</table>";
 
         $con = null;
     } catch (PDOException $e) {
--- to-do-application.php
+++ to-do-application.php
@@ -8,24 +8,59 @@ include "crud.php"
 
 <head>
     <title>To-Do Application</title>
+    <style>
+        * {
+            font-family: "Times New Roman", Times, serif;
+            font-size: 16px;
+        }
+
+        h1 {
+            font-size: 24px;
+        }
+
+        table {
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            width: 200px;
+            height: 25px;
+            border: solid 1px;
+            border-collapse: collapse;
+            text-align: center;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        input {
+            border: solid 1px black;
+        }
+    </style>
 </head>
 
 <body>
     <h1>To-Do Application</h1>
 
+    <?php read() ?>
+
     <form action="to-do-application.php" method="POST">
-        <label for="task">Add your task</label>
-        <input type="text" name="text" required>
+        <a>Add your task</a>
+        <br>
+        <label for="name">Name</label>
+        <input type="text" name="name" required>
+        <label for="description">Description</label>
+        <input type="text" name="description">
+        <br>
         <input type="submit" name="add" value="Add">
     </form>
 
-    <?php read() ?>
-
     <?php
-    if (!empty($_POST["text"]))
-        create($_POST["text"])
+    if (!empty($_POST["name"]))
+        create($_POST["name"], $_POST["description"])
     ?>
-
 </body>
 
 </html>
