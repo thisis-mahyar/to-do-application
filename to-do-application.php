c4b78a6bd73d01abdfbcd1145a97d57b0997ea5b
Mahyar <mahyarstate@gmail.com>
2024-12-12 16:09:24 +0330

    add modal window with events

--- to-do-application.php
+++ to-do-application.php
@@ -27,10 +27,59 @@ include "crud.php"
         <input type="submit" name="add" value="Add">
     </form>
 
+    <div class="modal">
+        <div class="update-form-background"></div>
+        <form class="update-form" action="to-do-application.php" method="POST">
+            <h2>Add your task</h2>
+            <br>
+            <label for="update-name">Name</label>
+            <input type="text" name="update-name" required>
+            <br>
+            <label for="update-description">Description</label>
+            <input type="text" name="update-description" required>
+            <br>
+            <input id='update-id' name="update-id" type="hidden" />
+            <input class="modal-add" type="submit" name="add" value="Add">
+            <a href=# class="modal-cancel">Cancel</a>
+        </form>
+
+        <?php
+        if (!empty($_POST['update-name']) && !empty($_POST['update-description']) && !empty($_POST['update-id']))
+            update($_POST['update-name'], $_POST['update-description'], $_POST['update-id'])
+        ?>
+    </div>
+
     <?php
     if (!empty($_POST["name"]))
         create($_POST["name"], $_POST["description"])
     ?>
+
+    <script>
+        const f = function(e) {
+            const id = e.currentTarget.id.split('-')[1];
+
+            document.querySelector('#update-id').value = id
+
+            document.querySelector('.modal').style.display = 'grid';
+
+            // if you click the background, the modal window disappears
+            document.querySelector('.update-form-background').addEventListener('click', function() {
+                document.querySelector('.modal').style.display = 'none';
+            })
+
+            // if you click the cancel button, the modal window disappears
+            document.querySelector('.modal-cancel').addEventListener('click', function() {
+                document.querySelector('.modal').style.display = 'none';
+            })
+
+        }
+
+        const elements = document.querySelectorAll(".update")
+
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].addEventListener('click', f)
+        }
+    </script>
 </body>
 
 </html>
